113aac84a6448a0de0abfbfbb261dc952b46dd40
jieunn <jieun776121@gmail.com>
2024-07-27 21:46:28 +0900

    chore: 프로젝트 생성 및 prettier, eslint 적용

---
+++ eslint.config.mjs
@@ -0,0 +1,23 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+
+export default [
+	{ files: ['**/*.{js,mjs,cjs,jsx}'] },
+	{
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+			globals: {
+				...globals.browser,
+			},
+		},
+	},
+	pluginJs.configs.recommended,
+	pluginReact.configs.flat.recommended,
+];
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+	return <div>Golab home page</div>;
+}
+
+export default App;
