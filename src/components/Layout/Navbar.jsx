3360f6d37c1cbeb61ab7f46f9ac80aad4f22b02d
jieunn <jieun776121@gmail.com>
2024-07-28 14:37:08 +0900

    feat: 토큰 유무에 따른 navbar UI 변경

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -2,17 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+	// let token = undefined;
+	let token = 'token';
+
 	return (
-		<div className="h-[72px] absolute top-0 flex justify-around items-center w-full bg-white">
+		<div className={`h-[72px] absolute top-0 flex justify-between px-[15vw] items-center w-full bg-white`}>
 			<Link to="/">
 				<img src="/img/logo.svg" alt="메인 Logo" />
 			</Link>
+
 			<div className="flex-template gap-8 text-Blue-300">
-				<Link to="/">Home</Link>
-				<Link to="/applicants">지원자 List</Link>
-				<Link to="/mylists">작성한 글</Link>
+				{token && (
+					<>
+						<Link to="/">Home</Link>
+						<Link to="/applicants">지원자 List</Link>
+						<Link to="/mylists">작성한 글</Link>
+					</>
+				)}
 				<Link to="/login" className="bg-Blue-300 py-2 px-4 rounded-xl text-white">
-					로그인
+					{token ? '로그아웃' : '로그인'}
 				</Link>
 			</div>
 		</div>
