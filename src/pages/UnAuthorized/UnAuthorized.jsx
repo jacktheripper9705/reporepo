8f8a256c3cbc07452ec83e1bd3cd82b014d2a8ef
jieunn <jieun776121@gmail.com>
2024-07-28 14:18:35 +0900

    feat: unauthorized 페이지 구현

--- src/pages/UnAuthorized/UnAuthorized.jsx
+++ src/pages/UnAuthorized/UnAuthorized.jsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const UnAuthorized = () => {
-	return <>접근 권한이 없습니다. 로그인하러가기</>;
+	return (
+		<div className="flex flex-col gap-4 justify-center items-center">
+			<h1 className=" text-[50px]">접근 권한이 없습니다</h1>
+			<Link to="/login" className="bg-Blue-100 w-36 py-2 rounded-xl flex justify-center text-white">
+				로그인 하러 가기
+			</Link>
+		</div>
+	);
 };
 
 export default UnAuthorized;
