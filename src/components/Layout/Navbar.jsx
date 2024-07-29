1ca640e503dff757a158e3fb3d87dbc058fe5d82
jieunn <jieun776121@gmail.com>
2024-07-29 16:04:49 +0900

    style: navbar ui 변경 반영

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -6,20 +6,32 @@ const Navbar = () => {
 	let token = 'token';
 
 	return (
-		<div className={`h-[72px] absolute top-0 flex justify-between px-[15vw] items-center w-full bg-white`}>
+		<div className={`h-[72px] absolute top-0 flex justify-between px-[15vw] items-center w-full z-10`}>
 			<Link to="/">
 				<img src="/img/logo.svg" alt="메인 Logo" />
 			</Link>
 
-			<div className="flex-template gap-8 text-Blue-300">
+			<div className="flex-template gap-5 text-Blue-400">
 				{token && (
 					<>
-						<Link to="/">Home</Link>
-						<Link to="/applicants">지원자 List</Link>
-						<Link to="/mylists">작성한 글</Link>
+						<Link to="/" className="flex gap-2">
+							<img src="/img/navbarHome.svg" />
+							Home
+						</Link>
+						<img src="/img/navbarLine.svg" />
+						<Link to="/applicants" className="flex gap-2">
+							<img src="/img/navbarPar.svg" />
+							지원자 List
+						</Link>
+						<img src="/img/navbarLine.svg" />
+						<Link to="/mylists" className="flex gap-2">
+							<img src="/img/navbarPost.svg" />
+							작성한 글
+						</Link>
+						<img src="/img/navbarLine.svg" />
 					</>
 				)}
-				<Link to="/login" className="bg-Blue-300 py-2 px-4 rounded-xl text-white">
+				<Link to="/login" className="bg-Blue-200 py-2 px-4 rounded-3xl text-white">
 					{token ? '로그아웃' : '로그인'}
 				</Link>
 			</div>
