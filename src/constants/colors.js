5639ff9a3572ab64cb4d69f55ede82e4bb9c6926
jieunn <jieun776121@gmail.com>
2024-07-29 15:47:32 +0900

    style: 디자인 시스템 수정 반영

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,8 +1,9 @@
 export const colors = {
 	'Blue-100': '#E0F8FF',
-	'Blue-200': '#00AEFF',
-	'Blue-300': '#0095FF',
-	'Blue-400': '#2563FF',
+	'Blue-200': '#10C7F9',
+	'Blue-300': '#00AEFF',
+	'Blue-400': '#0095FF',
+	'Blue-500': '#2563FF',
 	Black: '#000000',
 	Gray: '#666666',
 };
