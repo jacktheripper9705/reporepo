e9ddadc4d2f043db81b101590ab64e2dc1ddf2ab
jieunn <jieun776121@gmail.com>
2024-08-05 01:03:28 +0900

    feat: 모집글 확인하기 ui 구현

--- src/pages/Post/components/PostTitle.jsx
+++ src/pages/Post/components/PostTitle.jsx
@@ -1,4 +1,4 @@
-const PostTitle = () => {
+const PostTitle = ({ text }) => {
 	const goBack = () => {
 		window.history.back();
 	};
@@ -6,7 +6,7 @@ const PostTitle = () => {
 		<div className="flex pb-6 gap-3">
 			<img src="/img/backIcon.svg" alt="뒤로가기아이콘" onClick={goBack} className="cursor-pointer" />
 			<img src="/img/postIcon.svg" alt="문서아이콘" />
-			<p className="text-[25px]">모집 글 작성하기</p>
+			<p className="text-[25px]">{text}</p>
 		</div>
 	);
 };
